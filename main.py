0e2299ef8cac9b44cc0291d73f62cc492cc107e4
Unkown <119738516+Chara143@users.noreply.github.com>
2025-04-26 19:27:11 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
-from src.constants import DISPLAY_SIZE, MAX_FPS, SHOOT_EVENT
+from random import randint
+from src.constants import DISPLAY_SIZE, MAX_FPS, SHOOT_EVENT, SPAWN_EVENT
 from src.player import Player
 from src.bullet import Bullet
 from src.enemy import Enemy
@@ -16,9 +17,18 @@ def game(win, clock):
     bullet_image.fill('green')
     bullets = list()
 
+    enemy_image = pygame.Surface([50, 50])
+    enemy_image.fill('white')
     enemies = list()
 
+    pygame.time.set_timer(SPAWN_EVENT, 2000, 1)
+    difficulty = 0 
+
+
     while True:
+        difficulty += clock.get_time()
+
+
         #Обраточка собычай
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -26,6 +36,12 @@ def game(win, clock):
             elif event.type == SHOOT_EVENT:
                 b = Bullet(bullet_image, player.rect.midtop,10)
                 bullets.append(0)
+            elif event.type == SPAWN_EVENT:
+                millies = round(2000- difficulty/70)
+                pygame.time.set_timer(SPAWN_EVENT, millies, 1)
+
+                e = Enemy(enemy_image, [randint(50, DISPLAY_SIZE[0] - 50), - enemy_image.height], 5 + difficulty/35_000, round(10 + difficulty/7000))
+                enemies.append(e)
         
         player.update()
         for i in bullets.copy():
@@ -33,9 +49,16 @@ def game(win, clock):
             if not i.alive:
                 bullets.remove(i)
 
+        for i in enemies.copy():
+            i.update()
+            if not i.alive:
+                enemies.remove(i)
+
         win.fill((0, 0, 0))
         player.render(win)
         for u in bullets:
+            u.render(win)
+        for u in enemies:
             u.render(win)
         pygame.display.update()
         clock.tick(MAX_FPS)
--- src/constants.py
+++ src/constants.py
@@ -2,3 +2,4 @@ import pygame
 DISPLAY_SIZE = (800, 600)
 MAX_FPS = 60
 SHOOT_EVENT = pygame.event.custom_type()
+SPAWN_EVENT = pygame.event.custom_type()
